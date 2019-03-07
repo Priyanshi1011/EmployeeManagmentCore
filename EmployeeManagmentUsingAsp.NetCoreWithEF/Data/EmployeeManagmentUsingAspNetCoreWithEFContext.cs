784f9f05781a1874765f2799f91af248dc0bffdf
Priyanshi Kothari <priyanshi@promactinfo.com>
2019-03-07 16:52:52 +0530

    allfiles

---
+++ EmployeeManagmentUsingAsp.NetCoreWithEF/Data/EmployeeManagmentUsingAspNetCoreWithEFContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagmentUsingAsp.NetCoreWithEF.Models;
+
+namespace EmployeeManagmentUsingAsp.NetCoreWithEF.Models
+{
+    public class EmployeeManagmentUsingAspNetCoreWithEFContext : DbContext
+    {
+        public EmployeeManagmentUsingAspNetCoreWithEFContext (DbContextOptions<EmployeeManagmentUsingAspNetCoreWithEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EmployeeManagmentUsingAsp.NetCoreWithEF.Models.Department> Department { get; set; }
+
+        public DbSet<EmployeeManagmentUsingAsp.NetCoreWithEF.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ EmployeeManagmentUsingAsp.NetCoreWithEF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManagmentUsingAsp.NetCoreWithEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
